afca38b9ca594e9bf7d5acfb15fb7a5946b9fe0d
vicziani <viczian.istvan@gmail.com>
2018-12-14 11:37:43 +0100

    pytest tesztesetek

--- tests/jtechlog/fizzbuzz_test.py
+++
@@ -1,9 +0,0 @@
-import unittest
-
-from jtechlog.fizzbuzz import fizzbuzz
-
-
-class TestFizzbuzz(unittest.TestCase):
-
-    def test_for_many(self):
-        self.assertEqual(fizzbuzz(16), "1 2 fizz 4 buzz fizz 7 8 fizz buzz 11 fizz 13 14 fizzbuzz 16")
